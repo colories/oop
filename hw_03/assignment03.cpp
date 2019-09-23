d00559658bf739f4752bf37c76bcbdb5aa632410
colories <colories@naver.com>
2019-09-23 21:29:50 +0900

    Update assignment03.cpp

--- hw_03/assignment03.cpp
+++ hw_03/assignment03.cpp
@@ -13,16 +13,18 @@ int main(void)
 	char * copy = "is no no";
 	char name = 'a';	// 자신의 학번 4, 5, 6번째 자리 
 
-//	cout << "Name : " << (&name)+i << endl;
-	cout << "in rodata - copy의 주소 : \t" << (void*)copy << endl;
-	cout << "in heap - c[0]의 주소 : \t" << &c[0] << endl;
-	cout << "in heap - c[600]의 주소 : \t" << &c[600] << endl;
+	cout << "in rodata - copy의 참조주소 : \t" << copy << endl;
+	cout << "in heap - c[0]의 참조주소 : \t" << &c[0] << endl;
+	cout << "in heap - c[600]의 참조주소 : \t" << &c[600] << endl;
 	cout << "in Stack - target의 주소 : \t" << &target << endl;
-	cout << "in Stack - b[0]의 주소 : \t" << &b[0] << endl;
 	cout << "in Stack - b[982]의 주소 : \t" << &b[982] << endl;
+	cout << "in Stack - b[0]의 주소 : \t" << &b[0] << endl;
        	cout << "in Stack - a의 주소 : \t\t" << &a << endl;	
 	cout << "in Stack - i의 주소 : \t\t" << &i << endl;
+	cout << "in Stack - c의 주소 : \t\t" << &c << endl;
+	cout << "in Stack - copy의 주소 : \t" << &copy << endl;
 	cout << "in Stack - name의 주소 : \t" << (int*)&name << endl;
+	cout << "&name + i : " << &name+i << endl;
 
 	return 0;
 }
