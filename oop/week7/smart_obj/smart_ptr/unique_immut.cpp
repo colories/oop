3a1e1fdfda2866780d0332049956d7376c4aa020
colories <colories@naver.com>
2019-10-28 10:03:11 +0000

    201601983 jung-hyun Kim oop homework - week7
    smart_obj - shared_ref, unique_obj

---
+++ oop/week7/smart_obj/smart_ptr/unique_immut.cpp
@@ -0,0 +1,93 @@
+//
+// Created by 김혁진 on 14/10/2019.
+//
+
+#include "unique_immut.h"
+
+namespace ptr {
+
+	unique_immut::unique_immut() {
+		_mgr = new mgr();
+	}	
+
+	unique_immut::unique_immut(Object *obj) {
+    		_mgr = new mgr(obj);
+	}	
+
+	unique_immut::unique_immut(const unique_immut &immut) {
+		if (immut._mgr) {
+        		int val = immut._mgr->ptr->get();
+        		_mgr = new mgr(val);
+    		}
+	}	
+
+	unique_immut::~unique_immut() {
+		release();
+	}
+
+	Object* unique_immut::get() const {
+		if (this->_mgr->ptr == nullptr) {
+			return nullptr;
+		}
+		else {
+			return this->_mgr->ptr;
+		}
+	}
+
+	void unique_immut::release() {
+		if (this->_mgr->ptr != nullptr) {
+			this->_mgr->~mgr();
+		}
+	}
+
+	unique_immut unique_immut::operator+(unique_immut &unique) {
+		Object l = *(this->_mgr->ptr);
+		Object u = *(unique._mgr->ptr);
+		release();
+		unique.release();
+		
+		return unique_immut(new Object(l.get() + u.get()));
+	}
+
+	unique_immut unique_immut::operator-(unique_immut &unique) {
+		Object l = *(this->_mgr->ptr);
+		Object u = *(unique._mgr->ptr);
+		release();
+		unique.release();
+		
+		return unique_immut(new Object(l.get() - u.get()));
+	}	
+
+	unique_immut unique_immut::operator*(unique_immut &unique) {
+		Object l = *(this->_mgr->ptr);
+		Object u = *(unique._mgr->ptr);
+		release();
+		unique.release();
+		
+		return unique_immut(new Object(l.get() * u.get()));
+	}
+
+	unique_immut unique_immut::operator/(unique_immut &unique) {
+		Object l = *(this->_mgr->ptr);
+		Object u = *(unique._mgr->ptr);
+		release();
+		unique.release();
+		
+		return unique_immut(new Object(l.get() / u.get()));
+	}
+
+	Object* unique_immut::operator->() {
+		return this->_mgr->ptr;
+	}
+
+	unique_immut& unique_immut::operator=(unique_immut &r) {
+		if (this == &r) {
+			return *this;
+		}
+	
+		this->release();
+		*this = r;
+	
+		return *this;
+	}
+} // end of namespace ptr
